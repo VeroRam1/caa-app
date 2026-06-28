d509abb6e16c33ac993441001374de6c42f12c9a
VeroRam1 <verorama63@gmail.com>
2026-06-28 13:36:56 +0200

    changed title name and changes in cors for deployment

--- backend/src/main/java/com/caa/backend/config/CorsConfig.java
+++ backend/src/main/java/com/caa/backend/config/CorsConfig.java
@@ -2,6 +2,8 @@ package com.caa.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -10,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
